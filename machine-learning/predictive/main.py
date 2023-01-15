bb4d176dc97285913ebf0a7689d87b5d774dd9ed
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-15 18:31:30 +0300

    fit model with rqandom forest model

--- machine-learning/predictive/main.py
+++ machine-learning/predictive/main.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 dataset = pd.read_csv('hirable.csv')
 
@@ -59,3 +60,8 @@ X = dataset.drop(['status'], axis=1)
 y = dataset.status
 
 X_train, X_test, y_train, y_test = train_test_split(X, y,train_size=0.8,random_state=1)
+
+#fitting the model with random forest model
+
+model = RandomForestClassifier(n_estimators =100)
+model.fir(X_train, y_train)

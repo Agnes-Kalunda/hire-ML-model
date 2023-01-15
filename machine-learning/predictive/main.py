079821e236619ee93d5346de5e3e0f1e950524f8
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-15 18:24:56 +0300

    cleaned up dataset and dropped unwanted columns

--- machine-learning/predictive/main.py
+++ machine-learning/predictive/main.py
@@ -6,3 +6,36 @@ import numpy as np
 
 dataset = pd.read_csv('hirable.csv')
 
+# Cleaning up dataset
+
+dataset = dataset.drop([
+
+"sl_no",
+
+"ssc_p",
+
+"ssc_b",
+
+"hsc_p",
+
+"hsc_b",
+
+"hsc_s",
+
+"specialisation",
+
+"salary",
+
+"degree_t"
+
+], axis=1)
+
+dataset = dataset.rename(columns = {'degree_p': 'bsc', 'mba_p': 'msc'})
+
+dataset['gender'] = dataset.gender.replace(['M', 'F'], [1, 2])
+
+dataset['workex'] = dataset.workex.replace(['Yes', 'No'], [1, 0])
+
+dataset['status'] = dataset.status.replace(['Placed', 'Not Placed'], [1, 0])
+
+print (dataset)

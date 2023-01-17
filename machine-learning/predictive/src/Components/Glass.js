0da81e5fc67ebfe0245414199c3b4d8f669e1564
Agnes-Kalunda <agypeter97@gmail.com>
2023-01-17 20:04:16 +0300

    defined flass fun

---
+++ machine-learning/predictive/src/Components/Glass.js
@@ -0,0 +1,14 @@
+import './Glass.css'
+import { useState } from 'react'
+import axios from 'axios'
+
+
+
+function Glass(){
+    const [ gender, setGender ] = useState('')
+    const [ bsc, setBsc ] = useState('')
+    const [workex, setWorkex] = useState('')
+    const [ etest_p, setEtest_p] = useState('')
+    const [msc, setMsc] = useState('')
+    
+}
